e8243be9da09e5a05c2bbfbf3f46024bcae352d6
k0 <k0@k0src.com>
2024-04-02 16:20:54 -0500

    django frmawork

---
+++ wsib/main/static/main/js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('searchForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        var inputValue = document.getElementById('searchInput').value;
+        console.log("Input value:", inputValue); 
+
+        fetch('/process_input/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken')
+            },
+            body: JSON.stringify({'input_text': inputValue})
+        })
+        .then(response => {
+            console.log('Response received:', response);
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data received:', data);
+            document.getElementById('result').innerText = data.processed_text;
+        })
+        .catch(error => console.error('Error:', error));
+    });
+});
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
---
+++ wsib/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def process_input(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        input_text = data.get('input_text', '')
+        processed_text = input_text.upper()  # Process the input text (in this case, converting to uppercase)
+        return JsonResponse({'processed_text': processed_text})
+    else:
+        return JsonResponse({'error': 'Invalid request'})
